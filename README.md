a5f99c8c88f1a0bfc09a8432d134547b7d4c9e84
kerny97160 <laguerre.kerny@gmail.com>
2023-06-12 21:19:32 -0400

    amélioration de l'UI

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Portfolio Developpeur Web
 
 Mon Portfolio développée avec AstroJS
+
+![Mon portfolio](kerny-laguere-portfolio.png)
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,23 +8,11 @@ function handleForm (e) {
 
 export function Contact () {
     return (
-        <form onSubmit={(e) => handleForm(e)}>
+        <form className="form-contact" onSubmit={(e) => handleForm(e)}>
             <div>
                 <input type="email" name="email" id="email" placeholder="Votre adresse électronique"/>
+                <button type="submit">Envoyer mon message</button>
             </div>
-            <div>
-                <input type="firstname" name="firstname" id="firstname" placeholder="Votre prénom."/>
-            </div>
-            <div>
-                <input type="lastname" name="lastname" id="lastname" placeholder="Votre nom de Famille"/>
-            </div>
-            <div>
-                <input type="sujet" name="sujet" id="sujet" placeholder="Votre sujet"/>
-            </div>
-            <div>
-                <textarea type="message" name="message" id="message" placeholder="Votre message..."></textarea>
-            </div>
-            <button type="submit">Envoyer mon message</button>
         </form>
     )
 }
